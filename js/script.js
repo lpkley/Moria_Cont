7bcfe691e047bb0b593aaf1a17e551a508d276cd
Felipe Soares <lpkleyy@hotmail.com>
2018-08-23 16:54:06 -0300

    Add Logo e corrigir erros

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
 
-    var toggleAffix = function(affixElement, scrollElement, wrapper) {
+    let toggleAffix = function(affixElement, scrollElement, wrapper) {
   
-        var height = affixElement.outerHeight(),
+        let height = affixElement.outerHeight(),
             top = wrapper.offset().top;
         
         if (scrollElement.scrollTop() >= (top - height)){
@@ -17,7 +17,7 @@ $(document).ready(function() {
   
 
     $('[data-toggle="affix"]').each(function() {
-        var ele = $(this),
+        let ele = $(this),
             wrapper = $('<div></div>');
         
         ele.before(wrapper);
@@ -30,37 +30,3 @@ $(document).ready(function() {
     });
   
 });
- // sandbox disable popups
- if (window.self !== window.top && window.name!="view1") {;
-    window.alert = function(){/*disable alert*/};
-    window.confirm = function(){/*disable confirm*/};
-    window.prompt = function(){/*disable prompt*/};
-    window.open = function(){/*disable open*/};
-  }
-  
-  // prevent href=# click jump
-  document.addEventListener("DOMContentLoaded", function() {
-    var links = document.getElementsByTagName("A");
-    for(var i=0; i < links.length; i++) {
-      if(links[i].href.indexOf('#')!=-1) {
-        links[i].addEventListener("click", function(e) {
-        console.debug("prevent href=# click");
-            if (this.hash) {
-              if (this.hash=="#") {
-                e.preventDefault();
-                return false;
-              }
-              else {
-                /*
-                var el = document.getElementById(this.hash.replace(/#/, ""));
-                if (el) {
-                  el.scrollIntoView(true);
-                }
-                */
-              }
-            }
-            return false;
-        })
-      }
-    }
-  }, false);
