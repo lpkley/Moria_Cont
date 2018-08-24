65aa51ff791903f3a093ebd9107a1a27944a40dd
Felipe Soares <lpkleyy@hotmail.com>
2018-08-24 00:01:25 -0300

    Add logo institucional e ajustar elementos do Nav

--- js/script.js
+++ js/script.js
@@ -4,19 +4,23 @@ $(document).ready(function() {
   
         let height = affixElement.outerHeight(),
             top = wrapper.offset().top;
-        
+                
         if (scrollElement.scrollTop() >= (top - height)){
             wrapper.height(height);
             affixElement.addClass("affix");
+            document.getElementById("mylogo").src = "img/Logo_inst_moria.svg";
+            document.getElementById("hamb_white").id = "hamb_black";
         }
         else {
             affixElement.removeClass("affix");
+            document.getElementById("mylogo").src = "img/Logo_white_moria.svg";
+            document.getElementById("hamb_black").id = "hamb_white";
+            
         }
       
     };
-  
 
-    $('[data-toggle="affix"]').each(function() {
+ $('[data-toggle="affix"]').each(function() {
         let ele = $(this),
             wrapper = $('<div></div>');
         
